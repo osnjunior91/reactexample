9a08ca17c86a796f937bde8e1620b610842750bc
Gold Models <goldmodelsadm@gmail.com>
2019-07-03 18:05:35 -0300

    Criação da Rotina de login usando reacy e firebase

--- reactexample/src/Login/index.js
+++ reactexample/src/Login/index.js
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
+import firebase from '../firebaseConnection';
 import './index.css';
 
-function Login(){
-return(
-    <div className="login">
-        <div className="jumbotron">
-           <form>
-           <div class="form-group">
-                <label for="usr">Email:</label>
-                <input type="text" class="form-control" id="usr" />
-            </div>
-            <div class="form-group">
-                <label for="pwd">Senha:</label>
-                <input type="password" class="form-control" id="pwd" />
-            </div>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">Login</a></p><br/>
-           </form>
-        </div>
-    </div>  
-);
-}
 
-export default Login;
+export default class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            senha: ''
+        };
+        this.cadastrarUsuario = this.cadastrarUsuario.bind(this);
+        this.loginUsuario = this.loginUsuario.bind(this);
+        this.changeInput = this.changeInput.bind(this);
+    }
+
+    loginUsuario(e){
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.senha)
+        .then((user) => {
+             alert(`Bem-vindo ${user.user.email}!`);
+        })
+        .catch((error) => {
+             alert(`Erro:  ${error}.`);
+        });
+ 
+    }
+
+    cadastrarUsuario(e){
+       firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.senha)
+       .then((user) => {
+            alert(`Usuário ${user.user.uid} criado com sucesso.`);
+       })
+       .catch((error) => {
+            alert(`Erro:  ${error}.`);
+       });
+        e.preventDefault();
+    }
+
+    changeInput(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render(){
+        return(
+            <div className="login">
+                <div className="jumbotron">
+                        <h1>
+                            Login
+                        </h1>
+                        <div class="form-group">
+                            <label for="usr">Email:</label>
+                            <input type="text" class="form-control" id="usr" name="email" value={this.state.email} 
+                            onChange={this.changeInput} />
+                            </div>
+                        <div class="form-group">
+                            <label for="pwd">Senha:</label>
+                            <input type="password" class="form-control" id="pwd" name="senha" value={this.state.senha} 
+                            onChange={this.changeInput} />
+                        </div>
+                        <br/>
+                        <button type="button" onClick={this.loginUsuario} class="btn btn-primary btn-lg btn-block">Login</button>
+                        <br/>
+                        <button type="button" onClick={this.cadastrarUsuario} class="btn btn-secondary btn-lg btn-block">Cadastrar usuário</button>
+                </div>
+            </div>  
+        );
+    }
+}
---
+++ reactexample/src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "***************",
+    authDomain: "************",
+    databaseURL: "*************",
+    projectId: "***************",
+    storageBucket: "***************",
+    messagingSenderId: "****************",
+    appId: "*****************************"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+export default firebase;
